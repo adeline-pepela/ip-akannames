0ee651e93f45c9dd11072770337ff61f49841c39
adeline-pepela <adelinemakokha@gmail.com>
2022-03-03 16:11:16 +0300

    extracting date data

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ let gender,date;
 btn.addEventListener('click', (event) => {
     event.preventDefault();
     console.log("Clicked...");
-    gender = form.elements.gender;
-    date= form.elements.date;
+    gender = form.elements.gender.value;
+    date= form.elements.date.value;
     console.log(gender, date);
+    cc=date.slice(0,2);
+    yy=date.slice(2,4);
+    mm=date.slice(5,7);
+    dd=date.slice(8);
+    console.log(cc,yy,mm,dd);
 });
 const akan = document.getElementById("akan");
 akan.innerHTML= "anything"
