ccb562a4f3ed0c4dc869dcabc86fbc99f791f016
adeline-pepela <adelinemakokha@gmail.com>
2022-03-03 17:03:32 +0300

    adding akan names to the js file

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const btn = document.getElementById("submit");
 const form = document.getElementById("form");
-let gender,date;
+let gender,date, akanName;
 btn.addEventListener('click', (event) => {
     event.preventDefault();
     console.log("Clicked...");
     gender = form.elements.gender.value;
     date= form.elements.date.value;
     console.log(gender, date);
-    cc=date.slice(0,2);
-    yy=date.slice(2,4);
-    mm=date.slice(5,7);
-    dd=date.slice(8);
+    cc=parseInt(date.slice(0,2));
+    yy=parseInt(date.slice(2,4));
+    mm=parseInt(date.slice(5,7));
+    dd=parseInt(date.slice(8));
+    dayOfWeek = ((((cc/4)- ((2*cc)-1)) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd) % 7);
     console.log(cc,yy,mm,dd);
-});
-const akan = document.getElementById("akan");
-akan.innerHTML= "anything"
+    console.log(dayOfWeek);
+    dayOfWeek = Math.floor(dayOfWeek);
+    console.log(dayOfWeek);
+    const akan = document.getElementById("akan");
+   
 
+    const akanmale = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw","Kofi","Kwame"];
+    const akanfemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+    if(gender ==="male"){
+        akanName = akanmale[dayOfWeek]
+    }
+    else if(gender ==="female"){
+        akanName = akanfemale[dayOfWeek]
+    }
+    
+    akan.innerHTML= akanName;
+})
