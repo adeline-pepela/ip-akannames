a681d9cf16bebf6e422c7c351d04e6d6e6bac6ca
adeline-pepela <adelinemakokha@gmail.com>
2022-03-03 15:51:34 +0300

    editing js file

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const btn = document.getElementById("submit");
-btn.addEventListener('click', () => {
+const form = document.getElementById("form");
+let gender,date;
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
     console.log("Clicked...");
+    gender = form.elements.gender;
+    date= form.elements.date;
+    console.log(gender, date);
 });
 const akan = document.getElementById("akan");
 akan.innerHTML= "anything"
